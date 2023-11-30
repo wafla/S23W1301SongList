af4228190dbe10e23387ce3fe6d44f6786507f09
최연우 <jjangguzi@naver.com>
2023-11-30 13:18:57 +0900

    🚀 SongList 완성

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20211391/s23w1301songlist/Song.kt
@@ -0,0 +1,9 @@
+package kr.ac.kumoh.ce.s20211391.s23w1301songlist
+
+data class Song(
+    val id: Int,
+    val title: String,
+    val singer: String,
+    val rating: Int,
+    val lyrics: String
+)
